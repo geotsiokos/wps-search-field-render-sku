7e4b789b28d5140d56095c4fa05ec0b344d55ebc
George Tsiokos <gtsiokos@gmail.com>
2021-04-27 11:19:54 +0300

    initial

---
+++ wps-search-field-render-sku.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Plugin Name: WPS Search Field Render SKU 
+ * Plugin URI: https://github.com/geotsiokos/wps-search-field-render-sku
+ * Description: 
+ * Author: gtsiokos
+ * Author URI: https://netpad.gr/
+ * License: GPLv3
+ * Version: 1.0.0
+ *
+ * @author George Tsiokos
+ * @package wps-search-field-render-sku
+ * @since wps-search-field-render-sku 1.0.0
+ *
+ */
+
+if ( !defined( 'ABSPATH' ) ) {
+	exit;
+}
+add_filter( 'woocommerce_product_search_field_product_title', 'example_function_woocommerce_product_search_field_product_title', 10, 2 );
+function example_function_woocommerce_product_search_field_product_title( $title, $post_id ) {
+	$product = wc_get_product( $post_id );
+	if ( $product ) {
+		$product_sku = $product->get_sku();
+		$title .= '<span>' . $product_sku . '</span>';
+	}
+	return $title;
+}
